963863b231e96fe238b83b49676650342ba12260
zlsh09830123 <zlsh09830123@gmail.com>
2020-10-26 22:16:26 +0800

    Added some additional architecture

---
+++ app/views/pages/index.php
@@ -0,0 +1,4 @@
+<?php require_once(APPROOT . '/views/inc/header.php'); ?>
+    <h1><?php echo $data['title']; ?></h1>
+    <P>This is the TraversyMVC PHP framework.</P>
+<?php require_once(APPROOT . '/views/inc/footer.php'); ?>
